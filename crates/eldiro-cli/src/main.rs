cd3c712f648d847b075176ebd9c3e1e903cea687
leo <leo.henches@gmail.com>
2025-02-14 14:29:48 +0100

    wiped project, start part 10

--- crates/eldiro-cli/src/main.rs
+++ crates/eldiro-cli/src/main.rs
@@ -1,36 +1,3 @@
-use eldiro::parse;
-use eldiro::Env;
-use eldiro::Val;
-use std::io::{self, Write};
-
-fn main() -> io::Result<()> {
-    let mut stdout_handle = io::stdout();
-    let stdin_handle = io::stdin();
-    let mut input = String::new();
-    let mut env = Env::new();
-    loop {
-        write!(stdout_handle, " -> ")?;
-        stdout_handle.flush()?;
-        stdin_handle.read_line(&mut input)?;
-        match run(&input, &mut env) {
-            Ok(None) => (),
-            Ok(Some(val)) => writeln!(stdout_handle, "{}", val)?,
-            Err(msg) => write!(stdout_handle, "{}", msg)?,
-        }
-        input.clear();
-    }
-}
-
-fn run(input: &str, env: &mut Env) -> Result<Option<Val>, String> {
-    let parse = parse(&input)?;
-
-    let evaluated = parse
-        .eval(env)
-        .map_err(|err| format!("Evaluation error: {}\n", err))?;
-
-    if evaluated == Val::Empty {
-        Ok(None)
-    } else {
-        Ok(Some(evaluated))
-    }
+fn main() {
+    println!("Hello world");
 }
--- crates/eldiro/src/lib.rs
+++ crates/eldiro/src/lib.rs
@@ -1,34 +1,7 @@
-mod binding_def;
-mod env;
-mod expr;
-mod func_def;
-mod func_call;
-mod statement;
-mod utils;
-mod val;
-
-pub use env::Env;
-pub use val::Val;
-
-use statement::Statement;
-
-pub struct Parse(Statement);
-
-impl Parse {
-    fn new(statement: Statement) -> Self {
-        Parse(statement)
-    }
-
-    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
-        self.0.eval(env)
-    }
-}
-
-pub fn parse(s: &str) -> Result<Parse, String> {
-    let (s, statement) = Statement::new(s)?;
-    if s.is_empty() {
-        Err(format!("Leftover input after parsing: {}", s))
-    } else {
-        Ok(Parse::new(statement))
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
